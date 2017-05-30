dbfdbd500fb4faefed1249b028f5278196cd0183
Firestone <165plo@gmail.com>
2017-05-29 21:52:40 -0600

    Added ability to queue vids and investigate folders

--- hello.go
+++ hello.go
@@ -51,11 +51,36 @@ var (
 			
 		}
 	}
+	
+	function createVidPlaylist(){
+		var divs = document.getElementsByTagName("div");
+		var vidList = [];
+		for (var i=0;i<divs.length;i++){
+			var inputInfo = divs[i].getElementsByTagName("input");
+			var value = inputInfo[0].value;
+			if(value != ""){
+				var vid = divs[i].getElementsByTagName("a")[0].text;
+				var o = {"order":value, "vid":vid};
+				vidList.push(o);
+			}
+		}
+		vidList.sort(function sortVids(ob1, ob2){
+			return ob1.order - ob2.order;
+		});
+		
+		var videoList = "";
+		for (var i=0;i<vidList.length;i++){
+			videoList += vidList[i].vid +';';
+		}
+		document.location.href = document.location.origin+"/Watch?Vids=" + videoList;
+	}
+	
+	
 	</script>`
 )
 
 func main() {
-	http.HandleFunc("/List", List)
+	http.HandleFunc("/List/", List)
 	http.HandleFunc("/Watch", Watch)
 	http.Handle("/", http.FileServer(http.Dir("./Vids/")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -63,7 +88,11 @@ func main() {
 }
 
 func List(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, head + getVideos() + tail)
+	folderPath := "./Vids"
+	folderPath += r.URL.Path[len("/List"):]
+	fmt.Printf("%q\n", r.URL.Path)
+	fmt.Printf("%q\n", folderPath)
+	fmt.Fprintf(w, head + getVideos(folderPath) + script + tail)
 }
 
 func Watch(w http.ResponseWriter, r *http.Request){
@@ -72,22 +101,28 @@ func Watch(w http.ResponseWriter, r *http.Request){
 	queryMap := r.URL.Query();
 	fileToWatch := queryMap["Vids"][0];
 	fileText := ("<video width=\"400\" preload=\"none\" controls><source src=\""+fileToWatch+"\" type=\"video/mp4\">Your browser does not support HTML5 video.</video>")
+	
 	fmt.Fprintf(w, head + fileText + script + tail)
 }
 
-func getVideos() string{
+func getVideos(path string) string{
 	fileText:=""
-	files, err := ioutil.ReadDir("./Vids")
+	files, err := ioutil.ReadDir(path)
 	if(err != nil){
 		fileText = `<p>No Files Found</p>`
 	}
 	for _, file := range files {
+		fileText += "<div>"
+		fileText += "<input type=\"text\" name=\"order\" size=1>   "
 		if strings.Contains(strings.ToLower(file.Name()), "mp4"){
 			fileText += ("<a href=\"Watch?Vids="+file.Name()+"\">"+file.Name()+"</a>\n<br/>\n")
-		}else{
-			fileText += ("<p>" + file.Name() + "</p>\n<br/>\n")
+		} else if file.IsDir() {
+			fileText += ("<a href=\"" + strings.Replace(path, "./Vids", "/List", -1) + "/" + file.Name() + "\">"+file.Name()+"</a>\n<br/>\n")
+		} else{
+			fileText += ("<label>" + file.Name() + "</label>\n<br/>\n")
 		}
-		
+		fileText += "</div>"
 	}
+	fileText += "<button onclick=\"createVidPlaylist()\" type=\"button\">Play Videos</button>"
 	return fileText;
 }
